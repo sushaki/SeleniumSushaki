457d692f749fd31a8a53c0388e1f5cb442b7bdf4
sushaki <ivan.alex.sushak@gmail.com>
2021-09-04 11:59:04 +0300

    some changes in BaseClass

--- src/test/java/testSelenium/BaseClass.java
+++ src/test/java/testSelenium/BaseClass.java
@@ -8,25 +8,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 
 public class BaseClass {
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private WebDriverWait wait2;
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, 25);
+    WebDriverWait wait2 = new WebDriverWait(driver, 7);
 
-    @BeforeSuite
-    public void beforeSuite() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 25);
-        WebDriverWait wait2 = new WebDriverWait(driver, 7);
-    }
 
     @BeforeTest
     public void beforeTest() {
+        WebDriverManager.chromedriver().setup();
+
         driver.manage().window().maximize();
         String urlSeleniumTestSite = "https://www.seleniumeasy.com/test/";
         driver.get(urlSeleniumTestSite);
--- src/test/java/testSelenium/Test1LabSelenium.java
+++ src/test/java/testSelenium/Test1LabSelenium.java
@@ -1,34 +1,25 @@
 package testSelenium;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
-public class Test1LabSelenium {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private WebDriverWait wait2;
+public class Test1LabSelenium extends BaseClass {
 
     @Test
     public void firstTestSelenium() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 25);
-        WebDriverWait wait2 = new WebDriverWait(driver, 7);
-        driver.manage().window().maximize();
-        String urlSeleniumTestSite = "https://www.seleniumeasy.com/test/";
-        driver.get(urlSeleniumTestSite);
-        WebElement closeLightBox = wait.until(ExpectedConditions.elementToBeClickable
-                (By.xpath("//*[@id='at-cv-lightbox-close']")));
-        closeLightBox.click();
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//        WebDriverWait wait = new WebDriverWait(driver, 25);
+//        WebDriverWait wait2 = new WebDriverWait(driver, 7);
+//        driver.manage().window().maximize();
+//        String urlSeleniumTestSite = "https://www.seleniumeasy.com/test/";
+//        driver.get(urlSeleniumTestSite);
+//        WebElement closeLightBox = wait.until(ExpectedConditions.elementToBeClickable
+//                (By.xpath("//*[@id='at-cv-lightbox-close']")));
+//        closeLightBox.click();
         WebElement inputForms = wait2.until(ExpectedConditions.elementToBeClickable
                 (By.xpath("//div[@class='collapse navbar-collapse']//" +
                         "a[contains(text(),'Input Forms')]")));
@@ -48,12 +39,6 @@ public class Test1LabSelenium {
         String userMessageInput = wrightText.getText();
         Assert.assertEquals(userMessageOnScreen, userMessageInput,
                 "User text after entering must be equal to Text under Show Message button");
-    }
-
-    @AfterTest(alwaysRun = true)
-    public void afterTest() {
-        driver.quit();
-        System.out.println("Test is finished");
     }
 }
 
