5f19fa3c28adb34c40c51a8cb90a7be397f200d6
sushaki <ivan.alex.sushak@gmail.com>
2021-09-04 10:59:41 +0300

    rename third test

--- src/test/java/testSelenium/Test3LabSelenium.java
+++ src/test/java/testSelenium/Test3LabSelenium.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class TestSelenium {
+public class Test3LabSelenium {
     @Test
     public void firstTestSelenium() {
         WebDriverManager.chromedriver().setup();
